efdf5d95dd9b16081fbafa8d211e7252f6181f20
sagara dissanayake <39671491+sagarromer@users.noreply.github.com>
2021-10-06 08:09:48 -0700

    initial commit bootstrap configured main cmnt created

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import Login from './Login'
+function App() {
+  return (
+    <div className="App">
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
